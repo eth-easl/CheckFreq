9bef0e0650ea7b84a1db13e99bf74df0b674b00b
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2021-11-17 17:53:58 +0100

    Separate stdout and stderr stream.

--- crashes/proc_crashes.py
+++ crashes/proc_crashes.py
@@ -5,35 +5,44 @@ import os
 import signal
 import subprocess
 
-import logging
-
 logging.basicConfig(format='%(asctime)s %(levelname)-4s [%(filename)s] %(message)s',
-    level=logging.DEBUG)
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--interval', type=int, default=30,
                     help='crash interval in seconds')
-parser.add_argument('--out', type=str, required=True, help='prefix for stdout files')
+parser.add_argument('--stdout', type=str, default='stdout',
+                    help='prefix for stdout files')
+parser.add_argument('--stderr', type=str, default='stderr',
+                    help='prefix for stdout files')
 parser.add_argument('cmd', nargs=argparse.REMAINDER,
-                    help='command to run, comes after --')
+                    help='command to run after --')
 args = parser.parse_args()
 
 if len(args.cmd) == 0 or args.cmd[0] != '--':
-    raise ValueError(
-        'Bad argument for command to run, place it after the -- separator')
+    print('Bad argument for command to run, place it after the -- separator')
+    exit(1)
 
-cmd = args.cmd[1:]
+cmd = ' '.join(args.cmd[1:])
 logger.info("Command to be executed {}".format(cmd))
 
 crash_count = 0
 while True:
-    outfile = "{}.{}".format(args.out, crash_count)
-    outfd = open(outfile, 'w')
-    logger.info("Redirecting stdout and stderr to {}".format(outfile))
-    proc = subprocess.Popen(cmd, shell=True, stdout=outfd, stderr=subprocess.STDOUT, start_new_session=True)
-    time.sleep(args.interval)
+    stdoutfile = "{}.{}".format(args.stdout, crash_count)
+    stderrfile = "{}.{}".format(args.stderr, crash_count)
+    logger.info("Redirecting stdout to {}, stderr to {}".format(stdoutfile, stderrfile))
+    stdoutfd = open(stdoutfile, 'w')
+    stderrfd = open(stderrfile, 'w')
+    proc = subprocess.Popen(cmd, shell=True, stdout=stdoutfd,
+                            stderr=stderrfd, start_new_session=True)
+    try:
+        time.sleep(args.interval)
+    except KeyboardInterrupt:
+        os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+        raise
+
     ret_code = proc.poll()
     if ret_code is not None:
         logger.info("Process terminated with exit code {}".format(ret_code))
@@ -41,5 +50,6 @@ while True:
 
     logger.info("Sending SIGKILL to process")
     os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
-    outfd.close()
+    stdoutfd.close()
+    stderrfd.close()
     crash_count += 1
