83e3d92038e71ff56aab5f117ae6980c42a03681
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2021-11-27 11:44:37 +0100

    Use Popen.wait.

--- crashes/proc_crashes.py
+++ crashes/proc_crashes.py
@@ -1,11 +1,11 @@
 import argparse
-import time
 import logging
 import os
 import signal
 import subprocess
-from scipy.stats import rv_continuous
+
 import numpy as np
+from scipy.stats import rv_continuous
 
 logging.basicConfig(format='%(asctime)s %(levelname)-4s [%(filename)s] %(message)s',
                     level=logging.DEBUG)
@@ -31,6 +31,7 @@ if len(args.cmd) == 0 or args.cmd[0] != '--':
 cmd = ' '.join(args.cmd[1:])
 logger.info("Command to be executed: {}".format(cmd))
 
+
 class failure_dist(rv_continuous):
     def __init__(self, t1, t2, b, A):
         super().__init__(a=0)
@@ -42,35 +43,36 @@ class failure_dist(rv_continuous):
     def _cdf(self, t):
         return self.A * (1 - np.exp(-t / self.t1) + np.exp((t - self.b) / self.t2))
 
+
 crash_count = 0
 interval = args.interval
 f_fail = failure_dist(t1=1.0, t2=0.8, A=0.5, b=24)
 
 while True:
     if (args.goog):
-        interval = f_fail.rvs() / 24 * 3600 # scale to 1hr
+        interval = f_fail.rvs() / 24 * 3600  # scale to 1hr
 
     stdoutfile = "{}.{}".format(args.stdout, crash_count)
     stderrfile = "{}.{}".format(args.stderr, crash_count)
     logger.info("Crash {} after {} sec.".format(crash_count, interval))
-    logger.info("Redirecting stdout to {}, stderr to {}".format(stdoutfile, stderrfile))
+    logger.info("Redirecting stdout to {}, stderr to {}".format(
+        stdoutfile, stderrfile))
     stdoutfd = open(stdoutfile, 'w')
     stderrfd = open(stderrfile, 'w')
     proc = subprocess.Popen(cmd, shell=True, stdout=stdoutfd,
                             stderr=stderrfd, start_new_session=True)
     try:
-        time.sleep(interval)
+        ret_code = proc.wait(timeout=interval)
+    except subprocess.TimeoutExpired:
+        logger.info("Sending SIGKILL to process")
+        os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+        crash_count += 1
     except KeyboardInterrupt:
         os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
         raise
-
-    ret_code = proc.poll()
-    if ret_code is not None:
+    else:
         logger.info("Process terminated with exit code {}".format(ret_code))
         break
-
-    logger.info("Sending SIGKILL to process")
-    os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
-    stdoutfd.close()
-    stderrfd.close()
-    crash_count += 1
+    finally:
+        stdoutfd.close()
+        stderrfd.close()
